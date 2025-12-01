c40f9148cdb1f265405245238cc8776c91b4adf0
Paran-oid <azizhmid10000@gmail.com>
2025-12-01 05:26:34 +0100

    follow up

--- Core/Inc/App/hal_init.hpp
+++ Core/Inc/App/hal_init.hpp
@@ -11,16 +11,6 @@ extern "C"
 /***************************************************************
  * Forward Declarations
  ***************************************************************/
-namespace scal
-{
-
-class UART;
-class SPI;
-
-template <typename T>
-class PTimer;
-
-}  // namespace scal
 
 /***************************************************************
  * HAL Generated Variables from "main.c"
@@ -30,21 +20,3 @@ extern SPI_HandleTypeDef hspi1;
 extern TIM_HandleTypeDef htim2;
 
 extern UART_HandleTypeDef huart2;
-
-/***************************************************************
- * Exported C++ Converted Peripherals
- ***************************************************************/
-extern scal::SPI spi1;
-
-extern scal::PTimer<uint32_t> tim2;
-
-extern scal::UART uart2;
-
-namespace stm_sd
-{
-/***************************************************************
- * Function Declarations
- ***************************************************************/
-void HALInit();
-
-}  // namespace stm_sd
